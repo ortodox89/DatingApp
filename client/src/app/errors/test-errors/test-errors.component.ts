3e21172bc1a95141618fe3c3687a461074cb8778
ortodox89 <ortodox89@gmail.com>
2022-11-02 21:21:01 +0100

    Errorhandeling

---
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-errors',
+  templateUrl: './test-errors.component.html',
+  styleUrls: ['./test-errors.component.css']
+})
+export class TestErrorsComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.error();
+    })
+  }
+
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.error();
+    })
+  }
+  
+  get401Error() {
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.error();
+    })
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.error();
+    })
+  }
+
+    get500Error() {
+    this.http.get(this.baseUrl + 'account/register', {}).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.error();
+    })
+  }
+}
