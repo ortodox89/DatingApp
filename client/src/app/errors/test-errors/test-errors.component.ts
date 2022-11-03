3f35150eb39df0c9ec58eee3b76c322c73098b1b
ortodox89 <ortodox89@gmail.com>
2022-11-03 19:56:42 +0100

    End of section 7

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestErrorsComponent implements OnInit {
   baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -18,15 +19,16 @@ export class TestErrorsComponent implements OnInit {
     this.http.get(this.baseUrl + 'buggy/bad-request').subscribe(response => {
       console.log(response);
     }, error => {
-      console.error();
+      console.log(error);
     })
   }
 
   get400ValidationError() {
-    this.http.get(this.baseUrl + 'buggy/not-found').subscribe(response => {
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe(response => {
       console.log(response);
     }, error => {
-      console.error();
+      console.log(error);
+      this.validationErrors = error;
     })
   }
   
@@ -34,7 +36,7 @@ export class TestErrorsComponent implements OnInit {
     this.http.get(this.baseUrl + 'buggy/auth').subscribe(response => {
       console.log(response);
     }, error => {
-      console.error();
+      console.log(error);
     })
   }
 
@@ -42,15 +44,15 @@ export class TestErrorsComponent implements OnInit {
     this.http.get(this.baseUrl + 'buggy/not-found').subscribe(response => {
       console.log(response);
     }, error => {
-      console.error();
+      console.log(error);
     })
   }
 
     get500Error() {
-    this.http.get(this.baseUrl + 'account/register', {}).subscribe(response => {
+    this.http.get(this.baseUrl + 'buggy/server-error', {}).subscribe(response => {
       console.log(response);
     }, error => {
-      console.error();
+      console.log(error);
     })
   }
 }
